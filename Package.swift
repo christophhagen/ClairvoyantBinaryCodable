2e7cdc3721a32be7a1c7ebb17771222fd268e6c1
Christoph Hagen <github@christophhagen.de>
2024-04-13 11:27:26 +0200

    Add DocC dependency for documentation

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/christophhagen/BinaryCodable", from: "2.0.0"),
         .package(url: "https://github.com/christophhagen/Clairvoyant", from: "0.9.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
